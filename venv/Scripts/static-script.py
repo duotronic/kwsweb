1eca6715580434976046055a45faf21838d6267f
duotronic <mark_rettek@outlook.com>
2017-03-27 15:18:44 -0600

    updated static3 to 6.2 for heroku

--- venv/Scripts/static-script.py
+++ venv/Scripts/static-script.py
@@ -1,10 +1,10 @@
-#!C:\Users\mark_\PycharmProjects\kwsweb2\venv\Scripts\python.exe
-# EASY-INSTALL-ENTRY-SCRIPT: 'static3==0.6.1','console_scripts','static'
-__requires__ = 'static3==0.6.1'
+#!C:\Users\mark_\PycharmProjects\kwsweb\venv\Scripts\python.exe
+# EASY-INSTALL-ENTRY-SCRIPT: 'static3==0.6.2','console_scripts','static'
+__requires__ = 'static3==0.6.2'
 import sys
 from pkg_resources import load_entry_point
 
 if __name__ == '__main__':
     sys.exit(
-        load_entry_point('static3==0.6.1', 'console_scripts', 'static')()
+        load_entry_point('static3==0.6.2', 'console_scripts', 'static')()
     )
